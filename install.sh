5c883a2d37d1810a87d92cfd70d40c54b7e52df9
Mateus Souza <mateussouzaweb@gmail.com>
2022-05-28 13:55:41 -0300

    Updated installation for sudo less

--- install.sh
+++ install.sh
@@ -6,26 +6,28 @@ set -u
 VERSION="v0.1.2"
 REPOSITORY="https://github.com/mateussouzaweb/compactor"
 BINARY="${REPOSITORY}/releases/download/${VERSION}/compactor"
+BINARIES="$HOME/.local/bin"
 
 echo "[INFO] Installing compactor and dependencies..."
 
 # Install dependencies
 if [[ $(node -v) == "" ]]; then
-  sudo apt install -y nodejs npm
+  apt install -y nodejs npm
 fi
 
 if [[ $(/sbin/ldconfig -p | grep libjpeg) == "" ]]; then
-  sudo apt install -y libjpeg-progs
+  apt install -y libjpeg-progs
 fi
 
 # Install NPM packages
 p=( gifsicle jpegoptim-bin cwebp-bin optipng-bin sass terser typescript svgo html-minifier )
 for i in "${p[@]}"; do
-  npm list -g | grep "$i" || npm install --quiet -g "$i"
+  npm list -g | grep "$i" || npm install --silence -g "$i"
 done
 
 # Install compactor
-sudo wget $BINARY -O /usr/local/bin/compactor
-sudo chmod +x /usr/local/bin/compactor
+mkdir -p $BINARIES
+wget $BINARY -O $BINARIES/compactor
+chmod +x $BINARIES/compactor
 
 echo "[INFO] Compactor ${VERSION} installed!"
