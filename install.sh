524569519fd665c0969e69a1f0431a4e62f8014c
Mateus Souza <mateussouzaweb@gmail.com>
2022-05-29 11:05:59 -0300

    Incremented installation process

--- install.sh
+++ install.sh
@@ -6,24 +6,33 @@ set -u
 VERSION="v0.1.2"
 REPOSITORY="https://github.com/mateussouzaweb/compactor"
 BINARY="${REPOSITORY}/releases/download/${VERSION}/compactor"
-BINARIES="$HOME/.local/bin"
+BINARIES="${HOME}/.local/bin"
 
 echo "[INFO] Installing compactor and dependencies..."
 
 # Install dependencies
 if [[ $(node -v) == "" ]]; then
+  echo "[INFO] Installing NodeJS and NPM..."
   apt install -y nodejs npm
 fi
 
 if [[ $(/sbin/ldconfig -p | grep libjpeg) == "" ]]; then
+  echo "[INFO] Installing libjpeg..."
   apt install -y libjpeg-progs
 fi
 
 # Install NPM packages
-p=( gifsicle jpegoptim-bin cwebp-bin optipng-bin sass terser typescript svgo html-minifier )
-for i in "${p[@]}"; do
-  npm list -g | grep "$i" || npm install --silence -g "$i"
+PKGS=( gifsicle jpegoptim-bin cwebp-bin optipng-bin sass terser typescript svgo html-minifier )
+INSTALLED=$(npm list -g)
+
+echo "[INFO] Checking NPM packages..."
+for PKG in "${PKGS[@]}"; do
+  echo $INSTALLED | grep "${PKG}" || npm install --silent -g "${PKG}"
+  echo "[INFO] ${PKG} - OK"
 done
+
+# Make sure binaries path works
+export PATH="$PATH:$BINARIES"
 
 # Install compactor
 mkdir -p $BINARIES
